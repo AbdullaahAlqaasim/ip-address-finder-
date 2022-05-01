c1e08d0c6c3e871845922d81a20d9c310eeada52
AbdullaahAlqaasim <67371047+AbdullaahAlqaasim@users.noreply.github.com>
2022-05-01 11:07:04 +0400

    Update ipaddressfinder.py

--- ipaddressfinder.py
+++ ipaddressfinder.py
@@ -13,3 +13,14 @@ print("Your Computer Name is:" + hostname)
 
 # printing IP Address
 print("Your Computer IP Address is:" + IPAddr) 
+
+# program made by abdullaah alqaasim 
+
+#my github 
+#https://github.com/AbdullaahAlqaasim
+
+#my instagram 
+#https://www.instagram.com/abdullaah_alqaasim/
+
+#my linked in 
+#https://www.linkedin.com/in/abdullaah-alqaasim-30b960209/
